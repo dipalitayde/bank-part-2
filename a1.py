69eb14f64a1dca7ec0a0b2966eba6639b7339838
dipalitayde <dipalitayde2009@gmail.com>
2025-02-14 18:33:21 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,28 @@
+def intro(name):
+    print("you are so sweet💕",name )
+    print("I am very glad to meet you 🥰, can we be friends?",name)
+
+user_name=input("Enter your name")
+intro(user_name)
+
+print("i am here to solve your queries regarding maths")
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+num1=int(input("enter number 1♥️"))
+num2=int(input("enter number 2♥️"))
+
+print("sum:", add(num1,num2))
+print("difference:", subtract(num1,num2))
+print("product:", multiply(num1,num2))
+print("quotient:", divide(num1,num2))
